8a586df3212aef950a865e683142d72c5236e574
Malia-Labor <malia.labor@revature.net>
2021-10-25 10:38:08 -0700

    code challenge for 10/25

---
+++ Code_Challenge_10_25/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Code_Challenge_10_25
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            try
+            {
+                string[] input = (Console.ReadLine()).Split('/');
+                int numerator = Int32.Parse(input[0]);
+                int denominator = Int32.Parse(input[1]);
+                ReduceFraction(numerator, denominator);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static void ReduceFraction(int numerator, int denominator)
+        {
+            int gcd;
+            gcd = GreatestCommonDivisor(numerator, denominator);
+
+            numerator = numerator / gcd;
+            denominator = denominator / gcd;
+
+            if (denominator == 1)
+                Console.WriteLine(numerator);
+            else
+                Console.WriteLine($"{numerator}/{denominator}");
+        }
+
+        static int GreatestCommonDivisor(int a, int b)
+        {
+            if (b == 0)
+                return a;
+            return GreatestCommonDivisor(b, a % b);
+        }
+    }
+}
